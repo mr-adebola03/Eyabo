8b2754f38af3e21fd5100eeab2be7baef119402e
mr-adebola03 <159397310+mr-adebola03@users.noreply.github.com>
2025-08-27 17:41:15 +0100

    set up back of eyabo

---
+++ src/main/java/com/cwa/app_eyabo/AppEyaboApplication.java
@@ -0,0 +1,13 @@
+package com.cwa.app_eyabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEyaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEyaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwa/app_eyabo/AppEyaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwa.app_eyabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEyaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
